3bb1a3c38a2c856f6a01e2c462f1599cc6f15a90
Purushottam Kafle <32263675+PurushottamKafle@users.noreply.github.com>
2018-05-04 17:38:07 +0545

    Update ClientSocket.java

--- ClientSocket.java
+++ ClientSocket.java
@@ -1,1 +1,26 @@
 
+package pksoft;
+import java.net.*;
+import java.io.*;
+
+public class ClientSocket {
+
+	public static void main(String args[]) throws Exception { // this throws exception
+		
+		String ip ="localhost"; // making ip address
+		int port = 9999; // making port
+		
+		Socket s = new Socket(ip, port); // passing port and ip to the sockets
+		
+		String str ="First Name: Purushottam"; // creating String data
+		
+		OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream());
+		// OutputStreamWriter helps to pass output to the server sockets
+		
+		PrintWriter out = new PrintWriter(os); // this is important to print
+		
+		os.write(str); // this helps to write to Server Socket
+		
+        os.flush(); // this is important for flush();
+	}
+}
