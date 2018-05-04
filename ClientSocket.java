49502300fabb7ea3aae4692a7ab9d5c3687a5c57
Purushottam Kafle <32263675+PurushottamKafle@users.noreply.github.com>
2018-05-04 17:53:20 +0545

    Update ClientSocket.java

--- ClientSocket.java
+++ ClientSocket.java
@@ -1,13 +1,14 @@
-
 package pksoft;
 import java.net.*;
 import java.io.*;
 
 public class ClientSocket {
 
-	public static void main(String args[]) throws Exception { // this throws exception
-		
+	public static void main(String args[]) { 
+		try { // trying to be happen something wrong
+			
 		String ip ="localhost"; // making ip address
+		
 		int port = 9999; // making port
 		
 		Socket s = new Socket(ip, port); // passing port and ip to the sockets
@@ -22,5 +23,12 @@ public class ClientSocket {
 		os.write(str); // this helps to write to Server Socket
 		
         os.flush(); // this is important for flush();
+        
+        os.close(); // this is important to close();
+        
+		}catch(Exception e) { // if anything goes wrong 
+			// this happens
+			System.out.println("Something is not right on Client Server...");
+		}
 	}
 }
